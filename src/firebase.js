411b4b7377b14d397fafa6628206c33e94e2a950
mamamun009 <mamamun1999@gmail.com>
2023-02-17 14:33:19 +0600

    leatest11

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from "firebase"
+import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyBEClrHTjdaL8pINxkSVXcVTjrh3cHhPVE",
-    authDomain: "facebook-clone-963fa.firebaseapp.com",
-    projectId: "facebook-clone-963fa",
-    storageBucket: "facebook-clone-963fa.appspot.com",
-    messagingSenderId: "68140238688",
-    appId: "1:68140238688:web:d212498059037b1f444005"
-  };
+  apiKey: "AIzaSyAZWOmFm3u6Jy-Vn8VJzSgHo-TAQXUB5dY",
+  authDomain: "connect-xt.firebaseapp.com",
+  projectId: "connect-xt",
+  storageBucket: "connect-xt.appspot.com",
+  messagingSenderId: "814311006603",
+  appId: "1:814311006603:web:0f74eafacca689649dcf8e",
+};
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
-export {auth, provider};
+export { auth, provider };
 export default db;
